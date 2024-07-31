7714545810fadd16d20e841a8084db67e9421467
Alex Tweed <alexander.w.tweed@student.uts.edu.au>
2024-07-31 14:13:04 +1000

    update git

--- trivia/api/serializers.py
+++ trivia/api/serializers.py
@@ -6,5 +6,4 @@ from .models import Room
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip', 'created_at')
-        
+        fields = '__all__'
